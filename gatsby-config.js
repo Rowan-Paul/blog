4313a066ab003ebf5bcf89638114820427845764
Rowan-Paul <rpflynn80@gmail.com>
2021-01-03 19:06:01 +0100

    Removed Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,12 +50,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: `UA-179555605-1`,
-      },
-    },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
